dfc97fc0da4be77329d6dd698092be7d20925a1b
Fantoni Fabrizio <ffantoni@sii-saltstack.finmatica.local>
2019-03-21 01:08:02 +0100

    Add show single job with a lot of error check and color :)

--- saltstack_frontend/models/salt.py
+++ saltstack_frontend/models/salt.py
@@ -1,34 +1,40 @@
 import subprocess
 import json
 from functools import lru_cache
+import logging
 
+log = logging.getLogger(__name__)
 
 @lru_cache(maxsize=2)
 def get_jobs():
-  out = subprocess.run(['sudo','salt-run','jobs.list_jobs','search_function="state.apply"','--output=json'], 
-        stdout=subprocess.PIPE)
-  try:
-    data = json.loads(out.stdout.decode('utf8'))
-  except:
-    return {}
-  return data
+    cmd = ['sudo', 'salt-run', 'jobs.list_jobs', 'search_function="state.apply"', '--output=json']
+    out = subprocess.run(cmd, stdout=subprocess.PIPE)
+    try:
+        data = json.loads(out.stdout.decode('utf8'))
+    except ValueError:
+        log.warn("Error retrieving jobs with command {}".format(cmd.join(' ')))
+        return {}
+    return data
+
 
 @lru_cache(maxsize=32)
 def get_job(job_id):
-  out = subprocess.run(['sudo','salt-run','jobs.list_job',job_id,'--output=json'], 
-        stdout=subprocess.PIPE)
-  try:
-    data = json.loads(out.stdout.decode('utf8'))
-  except:
-    return {}
-  return data
+    cmd = ['sudo', 'salt-run', 'jobs.list_job', job_id, '--output=json']
+    out = subprocess.run(cmd, stdout=subprocess.PIPE)
+    try:
+        data = json.loads(out.stdout.decode('utf8'))
+    except ValueError:
+        log.warn("Error retrieving job detail with command {}".format(cmd.join(' ')))
+        return {}
+    return data
+
 
 @lru_cache(maxsize=2)
 def get_minions():
-  out = subprocess.run(['sudo','salt-run','manage.present','--output=json'], 
-        stdout=subprocess.PIPE)
-  try:
-    data = json.loads(out.stdout.decode('utf8'))
-  except:
-    return {}
-  return data
+    out = subprocess.run(['sudo', 'salt-run', 'manage.present', '--output=json'],
+                         stdout=subprocess.PIPE)
+    try:
+        data = json.loads(out.stdout.decode('utf8'))
+    except ValueError:
+        return {}
+    return data
--- saltstack_frontend/views/jobs.py
+++ saltstack_frontend/views/jobs.py
@@ -4,6 +4,7 @@ from .. import models
 
 import pprint
 
+
 @view_config(route_name='jobs', renderer='../templates/jobs_list.jinja2')
 def view_jobs(request):
     """ List all done jobs. using jobs.list_jobs salt command line utility
@@ -29,41 +30,66 @@ def view_job_details(request):
     params:
       id: url path parameter, referred to salt job id
       minion: GET parameter, optional name of the minion to see job results
+    return:
+      job_result: {'return': <value> } where value can be:
+      
     """
     minions_job_result = dict()
     minions_done_ok = 0
     job_id = request.matchdict['id']
     minion_id = request.params.get('minion', None)
-    
+
     # get output of salt-run jobs.list_job command
     job_data = models.salt.get_job(job_id)
 
-    for minion in job_data['Minions']:
+    for minion in job_data.get('Minions', {}):
         num_steps = 0
-        job_failed = True
+        job_failed = False
         if 'return' in job_data['Result'][minion]['return']:
             # salt-ssh put return inside other return
             job_result = job_data['Result'][minion]['return']
         else:
             job_result = job_data['Result'][minion]
-        if not isinstance(job_result, str):
+
+        if isinstance(job_result, str):
+            # ssh minions that give error immediately at first connection
+            job_result = {'return': [job_result]}
+            job_failed = True
+        else:
             if isinstance(job_result['return'], dict):
                 if '_error' in job_result['return']:
                     # ssh minion that give error in returning data
                     job_result['return'] = [job_result['return']['stderr']]
+                    job_failed = True
+                elif 'error' in job_result['return']:
+                    # minion that give error in returning data (like utf8 encoding)
+                    job_result['return'] = [job_result['return']]
+                    job_failed = True                    
                 else:
                     num_steps = len(job_result['return'].keys())
-                    # Used for frontend statistics
-                    minions_done_ok += 1
-                    job_failed = False
-        else:
-            # ssh minions that give error immediately at first connection
-            job_result = {'return': [job_result]}
+                    # Order output by __run_num__ by creating new dict: {sequence: step_data}
+                    # Need to create an index of internal salt name of steps, cause steps not done never have
+                    # a __id__ assigned.
+                    job_result['steps_id'] = {step['__run_num__']:step_name.split('_|-')[1] 
+                                              for step_name, step in job_result['return'].items()}
+                    job_result['return'] = {step['__run_num__']:step for step in job_result['return'].values()}
+
+                    # Analize step by step and mark failed
+                    for step in job_result['return']:
+                        # Find failed steps based on cmd.run (must evaluate retcode)
+                        if 'retcode' in job_result['return'][step]['changes']:
+                            if not job_result['return'][step]['changes']['retcode'] == 0:
+                               job_result['return'][step]['result'] = False
+                               job_failed = True
+
+        if not job_failed:
+            minions_done_ok += 1
+
         minions_job_result[minion] = {'result': job_result,
                                       'num_steps': num_steps,
                                       'failed': job_failed
                                       }
-    # pprint.pprint(minions_job_result)
+    #print(job_result)
     return {'minion_id': minion_id,
             'minions_total': len(minions_job_result),
             'minions_failed': len(minions_job_result) - minions_done_ok,
