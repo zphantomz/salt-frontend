9673fa59d4f1dee37b640ee55d3ff8b1f6eaf64e
Fantoni Fabrizio <ffantoni@sii-saltstack.finmatica.local>
2019-04-03 12:02:33 +0200

    Sistemati problemi di ordinamento e vista states onfail

--- saltstack_frontend/views/jobs.py
+++ saltstack_frontend/views/jobs.py
@@ -20,6 +20,7 @@ def view_jobs(request):
     refresh = request.params.get('refresh', None)
     if refresh:
         models.salt.get_jobs.cache_clear()
+        models.salt.get_job.cache_clear()
         return HTTPFound(request.route_url('jobs'))
     jobs = models.salt.get_jobs()
     return {'jobs': jobs}
@@ -41,7 +42,7 @@ def view_job_details(request):
     minions_done_ok = 0
     job_id = request.matchdict['id']
     minion_id = request.params.get('minion', None)
-
+   
     # get output of salt-run jobs.list_job command
     job_data = models.salt.get_job(job_id)
 
@@ -52,7 +53,7 @@ def view_job_details(request):
             # Handle minions without any return
             job_result = "Minion not return"
         elif 'return' in job_data['Result'][minion]['return']:
-            # salt-ssh put return inside other return
+            # salt-ssh put return inside other return key
             job_result = job_data['Result'][minion]['return']
         else:
             job_result = job_data['Result'][minion]
@@ -62,8 +63,16 @@ def view_job_details(request):
             log.info("Minion {} return with error".format(minion))
             minion_job_data = [job_result]
             job_failed = True
+        elif isinstance(job_result['return'], str):
+            # minion with no return
+            log.info("Minion {} return with error".format(minion))
+            minion_job_data = ["No Return"]
+            job_failed = True            
         else:
             if isinstance(job_result['return'], dict):
+                minion_job_data = dict()
+                step_with_exception = 0
+                num_steps = 0
                 if '_error' in job_result['return']:
                     # ssh minion that give error in returning data
                     minion_job_data = [job_result['return']['stderr']]
@@ -73,36 +82,44 @@ def view_job_details(request):
                     # minion that give error in returning data (like utf8 encoding)
                     minion_job_data = [job_result['return']]
                     job_failed = True
-
+                
                 else:
                     num_steps = len(job_result['return'].keys())
-
                     # Analize step by step order them by __run_num__ and mark failed and warning step
-                    minion_job_data = dict()
-                    step_with_exception = 0
                     for step, step_data in job_result['return'].items():
-                        # Find failed steps based on cmd.run (must evaluate retcode)
+                        # Steps based on cmd.run (must evaluate retcode to find failed steps)
                         if 'retcode' in step_data['changes']:
                             if not step_data['changes']['retcode'] == 0:
                                step_data['result'] = False
                                job_failed = True
+                               step_with_exception += 1
                         elif not '__run_num__' in job_result['return'][step]:
                             # Handle minion who give exception
                             step_data['result'] = False
                             job_failed = True
-                            job_result['return'][step]['__run_num__'] = (100 + step_with_exception)
+                            job_result['return'][step]['__run_num__'] = (1000 + step_with_exception)
+                            step_with_exception += 1
+                        elif not 'duration' in job_result['return'][step]:
+                            # Steps not executed (require, onfail etc...)
+                            step_data['result'] = None
+                        elif step_data['result'] == False:
+                            # Steps who return False (Failed steps)
+                            job_failed = True
                             step_with_exception += 1
                         else:
                             pass
+                        #log.info("Minion: {} step: {} failed: {}".format(minion, step.split('_|-')[1], job_failed))
                         minion_job_data[job_result['return'][step]['__run_num__']] = step_data
+                        # Alwais force a step name, not based on __id__ key, some time is missing...
                         minion_job_data[job_result['return'][step]['__run_num__']]['step_id'] = step.split('_|-')[1]
 
         if not job_failed:
             minions_done_ok += 1
-
+        #log.info("Minion: {} job_failed: {}".format(minion, job_failed))
         minions_job_result[minion] = {'data': minion_job_data,
                                       'num_steps': num_steps,
-                                      'failed': job_failed
+                                      'failed': job_failed,
+                                      'steps_failed': step_with_exception
                                       }
 
     return {'minion_id': minion_id,
